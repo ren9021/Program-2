9a26af8ee01d966e97bc941bc51dd8c8a57e4cd0
Renee White <43598859+ren9021@users.noreply.github.com>
2020-05-21 16:29:57 -0500

    Add files via upload

---
+++ Program-2.py
@@ -0,0 +1,69 @@
+#***************************************************************
+#
+#  Developer:         Renee White
+#
+#  Program #:         Program 2
+#
+#  File Name:         ReneeWhite_Program2
+#
+#  Course:            COSC 1336 Programming Fundamentals I 
+#
+#  Due Date:          09-13-2019
+#
+#  Instructor:        Fred Kumi 
+#
+#  Chapter:           Chapter # 2
+#
+#  Description:
+#     This program calculates the average of 5 test scores.
+#
+#***************************************************************
+
+#***************************************************************
+#
+#  Function:     main
+# 
+#  Description:  The main function of the program
+#
+#  Parameters:   None
+#
+#  Returns:      Nothing 
+#
+#**************************************************************
+def main():
+    
+    developerInfo()
+    
+    test1 = int(input('Enter the first test score: '))
+    test2 = int(input('Enter the second test score: '))
+    test3 = int(input('Enter the third test score: '))
+    test4 = int(input('Enter the fourth test score: '))
+    test5= int(input('Enter the fifth test score: '))
+    average = (test1 + test2 + test3 + test4 + test5) // 5
+    print('The average score is', average)
+    
+    
+    # End of the main function 
+    
+#***************************************************************
+#
+#  Function:     developerInfo
+# 
+#  Description:  Prints Programmer's information
+#
+#  Parameters:   None
+#
+#  Returns:      Nothing 
+#
+#**************************************************************
+def developerInfo():
+    print('Name:     Renee White')
+    print('Course:   Programming Fundamentals I')
+    print('Program:  2')
+    print()
+    # End of the developerInfo function
+
+# Call the main function
+main()
+
+# End of Program 1
